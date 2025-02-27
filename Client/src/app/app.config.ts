fc0ba334f333533dbec1ae89e7e62aeb894a0c43
Mustafa shawkey <92957187+Mustafa212@users.noreply.github.com>
2025-02-27 13:52:15 +0200

    login and register ui

--- Client/src/app/app.config.ts
+++ Client/src/app/app.config.ts
@@ -3,12 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHotToastConfig } from '@ngneat/hot-toast';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { jwtInterceptor } from './_interceptors/jwt.interceptor';
+import { errorInterceptor } from './_interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: 
   [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideHotToastConfig(),
+    provideHttpClient(withInterceptors([errorInterceptor,jwtInterceptor ])),
+
   ]
 };
